67d3e41444e50b5832493f670f8f9b647c95ddd0
Poushali Bhattacharyya <poushalibhattacharyya02@gmail.com>
2025-04-14 17:54:31 +0530

    back to start

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/games")
-def games():
-    return render_template("games.html")
-
-@app.route("/bot")
+@app.route('/bot')
 def bot():
     return render_template("bot.html")
 
-@app.route("/music")
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/games')
+def games():
+    return render_template("games.html")
+
+@app.route('/music')
 def music():
     return render_template("music.html")
 
-@app.route("/about")
+@app.route('/about')
 def about():
     return render_template("about.html")
-
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
